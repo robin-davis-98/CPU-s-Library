dfb5f2f550ecbc4361bad7961515aed06766a646
Me <ryda98@gmail.com>
2024-05-01 23:47:21 +1000

    inital commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Library Mod
+
+## Overview
+This mod is designed to hold functions that will be shared across my all my mods.
+
+This will most likely include multiblock handling, general functions and ideas for my mods.
---
+++ src/main/java/dev/robindavis/library/CpuLibraryDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.robindavis.library;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CpuLibraryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
